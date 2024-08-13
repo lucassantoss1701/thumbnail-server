33f1cc4c65fc166cac12fa202871a3a0670a9c3b
Lucas Santos <lucas.santoss1701@gmail.com>
2024-08-12 21:44:18 -0300

    feat: uploading images

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use std::net::SocketAddr;
 use axum::{Extension, Router};
-use axum::routing::get;
-use sqlx::Row;
+use axum::body::Body;
+use axum::extract::{Multipart, Path};
+use axum::http::{header, HeaderMap};
+use axum::response::{Html, IntoResponse};
+use axum::routing::{get, post};
+use sqlx::{Row};
+use tokio_util::io::{ReaderStream};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -19,7 +24,9 @@ async fn main() -> anyhow::Result<()> {
 
     // Build Axum with an "extension" to hold the database connection pool
     let app = Router::new()
-        .route("/", get(test))
+        .route("/", get(index_page))
+        .route("/upload", post(uploader))
+        .route("/image/:id", get(get_image))
         .layer(Extension(pool));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     axum_server::bind(addr)
@@ -37,4 +44,86 @@ async fn test(Extension(pool): Extension<sqlx::SqlitePool>) -> String {
         .unwrap();
     let count = result.get::<i64, _>(0);
     format!("{count} images in the database")
+}
+
+async fn index_page() -> Html<String> {
+    let path = std::path::Path::new("src/index.html");
+    let content = tokio::fs::read_to_string(path).await.unwrap();
+    Html(content)
+}
+
+async fn insert_image_into_database(pool: &sqlx::SqlitePool, tags: &str) -> anyhow::Result<i64>{
+    let row = sqlx::query("INSERT INTO images (tags) VALUES (?) RETURNING id")
+        .bind(tags)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(row.get(0))
+}
+
+
+
+async fn uploader(Extension(pool): Extension<sqlx::SqlitePool>, mut multipart: Multipart) -> String {
+    let mut tags = None; // "None" means "no tags yet"
+    let mut image = None;
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let name = field.name().unwrap().to_string();
+        let data = field.bytes().await.unwrap();
+
+        match name.as_str() {
+            "tags" => tags = Some(String::from_utf8(data.to_vec()).unwrap()), // Using Some means we can check we received it
+            "image" => image = Some(data.to_vec()),
+            _ => panic!("Unknown field: {name}"),
+        }
+    }
+
+    if let (Some(tags), Some(image)) = (tags, image) { // Destructuring both Options at once
+        let new_image_id = insert_image_into_database(&pool, &tags).await.unwrap();
+        save_image(new_image_id, &image).await.unwrap();
+    } else {
+        panic!("Missing field");
+    }
+
+    "Ok".to_string()
+}
+
+async fn save_image(id: i64, bytes: &[u8]) -> anyhow::Result<()> {
+    // Check that the images folder exists and is a directory
+    // If it doesn't, create it.
+    let base_path = std::path::Path::new("images");
+    if !base_path.exists() || !base_path.is_dir() {
+        tokio::fs::create_dir_all(base_path).await?;
+    }
+
+    // Use "join" to create a path to the image file. Join is platform aware,
+    // it will handle the differences between Windows and Linux.
+    let image_path = base_path.join(format!("{id}.jpg"));
+    if image_path.exists() {
+        // The file exists. That shouldn't happen.
+        anyhow::bail!("File already exists");
+    }
+
+    // Write the image to the file
+    tokio::fs::write(image_path, bytes).await?;
+    Ok(())
+}
+
+async fn get_image(Path(id): Path<i64>) -> impl IntoResponse {
+    let filename = format!("images/{id}.jpg");
+    let attachment = format!("filename={filename}");
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        header::CONTENT_TYPE,
+        header::HeaderValue::from_static("image/jpeg"),
+    );
+    headers.insert(
+        header::CONTENT_DISPOSITION,
+        header::HeaderValue::from_str(&attachment).unwrap()
+    );
+    let file = tokio::fs::File::open(&filename).await.unwrap();
+    axum::response::Response::builder()
+        .header(header::CONTENT_TYPE, header::HeaderValue::from_static("image/jpeg"))
+        .header(header::CONTENT_DISPOSITION, header::HeaderValue::from_str(&attachment).unwrap())
+        .body(Body::from_stream(ReaderStream::new(file)))
+        .unwrap()
 }
